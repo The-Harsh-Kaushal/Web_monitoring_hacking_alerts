06be046d215555a649af5df97983077ac2543cee
Harsh Kaushal <harshkaushal302@gmail.com>
2025-04-04 12:31:14 +0530

    backend starting

--- frontend/src/pages/Shielding.jsx
+++ frontend/src/pages/Shielding.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import ServerStatus from '../../components/ServerStatus'
 
 const Shielding = () => {
   return (
     <div>
         <Navbar/>
+        <ServerStatus/>
     </div>
   )
 }
